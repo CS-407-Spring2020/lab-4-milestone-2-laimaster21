805bdf1db13dd3a1a2c0802257fa31678e45b008
laimaster <cyuan42@wisc.edu>
2020-02-24 17:45:36 +0000

    working on m2

--- app/src/main/java/com/example/lab4milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone2/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.lab4milestone2;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startListening();
+
+        if(Build.VERSION.SDK_INT>=23 &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+        locationManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        Location location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location!=null){
+
+        }
+
+
+        locationListener=new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+
+
+
+    }
+    public void startListening(){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            locationManager=(LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        }
+
+
+
+    }
+    @Override
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            startListening();
+
+        }
+    }
+
+    public void updateLocationInfo(Location location){
+        Log.i("Location info",location.toString());
     }
 }
